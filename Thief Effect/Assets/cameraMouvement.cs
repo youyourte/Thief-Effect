ac24c6335bfeda23fc50c9acb6e5dbe63dc5f111
Youcef Sayad <youcef.sayad@epita.fr>
2019-01-27 12:03:54 +0100

    Rajout du player

---
+++ Thief Effect/Assets/cameraMouvement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMouvement : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public float mouseSmoothing;
+    public GameObject Player;
+    Vector2 mouseLook;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mousePosition = Vector2.Scale(mousePosition, new Vector2(mouseSensitivity * mouseSmoothing, mouseSensitivity * mouseSmoothing));
+        mouseLook += mousePosition;
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        Player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Player.transform.up);
+    }
+}
